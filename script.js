8453034596331fcb89953dba7edf09636ec25207
Steph Poleij <99033734@mydavinci.nl>
2021-11-30 19:25:38 +0100

    eerste commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var btn = document.createElement('button');
+btn.style.margin = '10px';
+btn.innerHTML = '...';
+document.body.appendChild(btn);
+
+// schijf hier tussen je code
+
+
+
+// schijf hier tussen je code
